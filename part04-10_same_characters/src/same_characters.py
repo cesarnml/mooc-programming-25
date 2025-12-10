0b703b7a7c9eb4e9bf3d8cb3775c4601a9934c2e
CesarNML <cesarnml@outlook.com>
2025-12-10 18:29:15 +0700

    complete p4s2

--- part04-10_same_characters/src/same_characters.py
+++ part04-10_same_characters/src/same_characters.py
@@ -1,4 +1,10 @@
 # Write your solution here
+def same_chars(word, index1, index2):
+    if index1 >= len(word) or index2 >= len(word):
+        return False
+    return word[index1] == word[index2]
+
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     print(same_chars("coder", 1, 2))
