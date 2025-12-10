b8317a71454a0ac20066d99430501dd739a26652
CesarNML <cesarnml@outlook.com>
2025-12-10 16:06:41 +0700

    minor

--- part04-04_square_of_hashes/src/square_of_hashes.py
+++ part04-04_square_of_hashes/src/square_of_hashes.py
@@ -1,8 +1,16 @@
 # Copy here code of line function from previous exercise
+def line(num, word):
+    if word == "":
+        print(num * "*")
+    else:
+        print(num * word[0])
+
 
 def square_of_hashes(size):
     # You should call function line here with proper parameters
-    line(4, "#")
+    for _ in range(size):
+        line(size, "#")
+
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
