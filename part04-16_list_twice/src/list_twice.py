7d219363bf25423003bc87ebb5c6486f51e495c8
CesarNML <cesarnml@outlook.com>
2025-12-17 22:21:27 +0700

    interim

--- part04-16_list_twice/src/list_twice.py
+++ part04-16_list_twice/src/list_twice.py
@@ -1,1 +1,11 @@
 # Write your solution here
+items = []
+while True:
+    item = int(input("New item: "))
+    if item == 0:
+        break
+    else:
+        items.append(item)
+    print(f"The list now: {items}")
+    print(f"The list in order: {sorted(items)}")
+print("Bye!")
