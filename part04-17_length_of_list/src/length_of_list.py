084b28195b007634a6f018840e0c58f3f3a35c08
CesarNML <cesarnml@outlook.com>
2025-12-18 11:43:18 +0700

    interim

--- part04-17_length_of_list/src/length_of_list.py
+++ part04-17_length_of_list/src/length_of_list.py
@@ -1,4 +1,8 @@
 # Write your solution here
+def length(a_list: list):
+    return len(a_list)
+
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
