a0ad9169c16fb30de2b33ae8fe0dab38a3c37569
CesarNML <cesarnml@outlook.com>
2026-01-13 12:24:40 +0700

    interim

--- part09-09_car/src/car.py
+++ part09-09_car/src/car.py
@@ -1,1 +1,18 @@
 # WRITE YOUR SOLUTION HERE:
+
+
+class Car:
+    def __init__(self):
+        self.__tank = 0
+        self.__odometer = 0
+
+    def __str__(self):
+        return f"Car: odometer reading {self.__odometer} km, petrol remaining {self.__tank} litres"
+
+    def fill_up(self):
+        self.__tank = 60
+
+    def drive(self, km: int):
+        can_drive = min(self.__tank, km)
+        self.__odometer += can_drive
+        self.__tank -= can_drive
