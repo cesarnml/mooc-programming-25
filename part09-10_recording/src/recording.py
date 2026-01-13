5e5b7c3e49fa0c23a8639a54a50867d8b47f5fb1
CesarNML <cesarnml@outlook.com>
2026-01-13 13:09:33 +0700

    complete part09;section03

--- part09-10_recording/src/recording.py
+++ part09-10_recording/src/recording.py
@@ -1,1 +1,16 @@
 # WRITE YOUR SOLUTION HERE:
+class Recording:
+    def __init__(self, length):
+        if length < 0:
+            raise ValueError("length can not be negative")
+        self.__length = length
+
+    @property
+    def length(self):
+        return self.__length
+
+    @length.setter
+    def length(self, length: int):
+        if length < 0:
+            raise ValueError("length can not be negative")
+        self.__length = length
--- part09-11_weather_station/src/weather_station.py
+++ part09-11_weather_station/src/weather_station.py
@@ -1,3 +1,21 @@
 # WRITE YOUR SOLUTION HERE:
+# typo
+class WeatherStation:
+    def __init__(self, name):
+        self.__name = name
+        self.__observations: list[str] = []
 
+    def __str__(self):
+        return f"{self.__name}, {self.number_of_observations()} observations"
 
+    def add_observation(self, observation: str):
+        self.__observations.append(observation)
+
+    def latest_observation(self):
+        if len(self.__observations) > 0:
+            return self.__observations[-1]
+        else:
+            return ""
+
+    def number_of_observations(self):
+        return len(self.__observations)
