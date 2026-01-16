4a3e59d9da67956ca2627492bba18abe8bbd8884
CesarNML <cesarnml@outlook.com>
2026-01-16 12:16:38 +0700

    interim

--- part09-12_service_charge/src/service_charge.py
+++ part09-12_service_charge/src/service_charge.py
@@ -1,3 +1,32 @@
 # WRITE YOUR SOLUTION HERE:
+class BankAccount:
+    def __init__(self, owner: str, account_no: str, balance: float):
+        self.__owner = owner
+        self.__account_no = account_no
+        self.__balance = 0
+        if balance > 0:
+            self.__balance = balance
+        else:
+            ValueError("balance cannot be negative")
 
+    def __service_charge(self):
+        fee = self.__balance * 0.01
+        self.__balance -= fee
 
+    @property
+    def balance(self):
+        return self.__balance
+
+    def deposit(self, amount: float):
+        if amount >= 0:
+            self.__balance += amount
+            self.__service_charge()
+        else:
+            raise ValueError("deposit amount must be positive")
+
+    def withdraw(self, amount: float):
+        if self.__balance > amount:
+            self.__balance -= amount
+            self.__service_charge()
+        else:
+            ValueError("balance insufficient")
--- part09-14_list_helper/src/list_helper.py
+++ part09-14_list_helper/src/list_helper.py
@@ -1,1 +1,25 @@
 # WRITE YOUR SOLUTION HERE:
+class ListHelper:
+    @classmethod
+    def greatest_frequency(cls, my_list: list[int]):
+        if not my_list:
+            return None
+
+        count = my_list.count(my_list[0])
+        most_common = my_list[0]
+        for num in my_list:
+            if my_list.count(num) > count:
+                count = my_list.count(num)
+                most_common = num
+        return most_common
+
+    @classmethod
+    def doubles(cls, my_list: list[int]):
+        count = 0
+        seen = []
+        for num in my_list:
+            if my_list.count(num) >= 2 and not num in seen:
+                count += 1
+            seen.append(num)
+
+        return count
